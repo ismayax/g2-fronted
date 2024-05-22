c5df3fd8c4802477317903f0eda609dcde530fc7
ismayax <ismailarfaoui14@gmail.com>
2024-05-22 12:02:00 +0200

    ismail

---
+++ src/screens/Roles.js
@@ -0,0 +1,61 @@
+import React, {useState} from "react";
+import {getAuth, createUserWithEmailAndPassword} from "../components/firebaseConfig";
+
+
+const auth = getAuth(firebaseApp);
+
+function Roles () {
+
+    const [isRegistrando, setIsRegistrando] = useState(false);
+
+    async function RegistrarUsuario(){
+
+    }
+
+       function submitHandler(e) {
+        e.precentDefault();
+
+        const email = e.target.elements.email.value;
+        const password = e.target.elements.password.value;
+        const rol = e.target.elements.rol.value;
+
+        console.log(
+            "submit", email, password, rol);
+       }
+       if(isRegistrando){
+
+        RegistrarUsuario()
+       }else{
+
+       }
+    return (
+        <div > 
+            <h1>{isRegistrando ? "Registrate" : "Inicia sesión" }</h1>
+            <form onSubmit={submitHandler}>
+                <label>
+                    correo electrónico:
+                    <input type="email" id="email"></input>
+                </label>
+            <label>
+                Contraseña:
+                <input type="password" id="password"></input>
+            </label>
+            <label>
+                Rol:
+                <select id="rol">
+                    <option value="admin">Administrador</option>
+                    <option value="docente">Docente</option>
+                    <option value="usuario">Usuario</option>
+                </select>
+                </label>
+
+               <input type="submit" value={isRegistrando ? "Registrar" : "Iniciar sesión"} ></input> 
+            </form>
+          
+                <button onClick={()=> setIsRegistrando(!isRegistrando)}>
+                    {isRegistrando ? "Ya tengo cuenta" : "Quiero registrarme"}
+                </button>
+          
+        </div>
+    )
+}
